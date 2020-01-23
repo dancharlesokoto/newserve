8d6a70fb87ce54fd033149e0dab8fb46555e4148
Dancharles Okoto <45866004+dancharlesokoto@users.noreply.github.com>
2020-01-23 10:17:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def load_model():
+    # model variable refers to the global variable
+    with open('salesmodel.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+@app.route('/')
+def home_endpoint():
+    return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    model = load_model()
+    # Works only for a single sample
+    if request.method == 'POST':
+        data = request.get_json()
+        prediction = np.array2string(model.predict(data))
+
+        return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    # load model at the beginning once only
+    app.run()
